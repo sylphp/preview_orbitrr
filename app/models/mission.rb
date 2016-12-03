7a71d8a25f64542594faf55d46947305ec213900
sylphp <louie.casas@outlook.com>
2016-12-03 21:13:28 +0800

    Added mission model validations

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,3 +1,6 @@
 class Mission < ApplicationRecord
 	belongs_to :user
+
+	validates :title, :description, :requirements, presence: true
+	validates :budget, numericality: {greater_than_or_equal_to: 0.01}, allow_blank: true
 end
